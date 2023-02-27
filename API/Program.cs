ffa889719cd53d7436f24a9653c2c2d3747eac12
Raluca-Gabriela Negru <raluca.negru@equilobe.com>
2023-02-27 20:01:59 +0200

    Creating the React project.

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 builder.Services.AddScoped<IActivityService, ActivityService>();
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Button, Header, List } from 'semantic-ui-react';
+
+function App() {
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:7286/api/Activity')
+            .then(response => {
+                setActivities(response.data);
+            })
+    }, [])
+
+
+    return (
+    <div>
+        <Header as='h2' icon='user' content='Reactivities' />
+        <List>
+            {activities.map((activity: any) => (
+                <List.Item key={activity.id}>
+                    {activity.title}
+                </List.Item>
+            ))}
+            </List>
+    </div>
+  );
+}
+
+export default App;
