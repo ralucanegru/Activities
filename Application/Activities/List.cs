958a64eb3c71514fe25369f04a15f535ee753347
Raluca-Gabriela Negru <raluca.negru@equilobe.com>
2023-02-27 21:24:31 +0200

    Created an query handler for geting a list of activities. Created an query handler for getting an ativity by id.

---
+++ Application/Activities/List.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Enitities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _dataContext;
+            public Handler(DataContext dataContext)
+            {
+                _dataContext = dataContext;
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _dataContext.Activities.ToListAsync();
+            }
+        }
+    }
+}
