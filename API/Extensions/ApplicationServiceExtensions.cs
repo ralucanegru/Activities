40657731e5f974bef8135e2ffd370f8cfa7e30b7
Raluca-Gabriela Negru <raluca.negru@equilobe.com>
2023-03-06 19:18:55 +0200

    Added a class ApplicationServiceExtensions for managing services in Program.cs.

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
