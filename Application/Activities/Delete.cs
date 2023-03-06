1fb31e49594dd521f008c28a78f9a4644ca0fb16
Raluca-Gabriela Negru <raluca.negru@equilobe.com>
2023-03-06 16:52:02 +0200

    Adding a delete handler for activity,

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Persistence.Data;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+
+            public Handler(DataContext dataContext)
+            {
+                _dataContext = dataContext;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _dataContext.Activities.FindAsync(request.Id);
+
+                _dataContext.Remove(activity);
+
+                await _dataContext.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
