e786f66d119e9e55413370c5c4f2eede58fd6780
Raluca-Gabriela Negru <raluca.negru@equilobe.com>
2023-03-06 16:43:49 +0200

    Adding an edit handler for activity. Adding AutoMapper for mapping 2 different entities.

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Application.Activities;
+using Application.Core;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
@@ -28,6 +29,7 @@ builder.Services.AddCors(opt =>
 });
 
 builder.Services.AddMediatR(typeof(List.Handler));
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Domain.Enitities;
+using MediatR;
+using Persistence.Data;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command :IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext dataContext, IMapper mapper)
+            {
+                _dataContext = dataContext;
+                _mapper = mapper;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _dataContext.Activities.FindAsync(request.Activity.Id);
+
+                _mapper.Map(request.Activity, activity);
+
+                await _dataContext.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
